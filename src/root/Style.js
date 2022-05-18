33af9813f1052aff786fe0e025cce747fb3d21af
RakhmatilloRR <rrr.engineer.94@gmail.com>
2022-05-18 19:09:53 +0500

    first message

--- src/root/Style.js
+++ src/root/Style.js
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
-  color: red;
-  font-size: 30px
+  color: green;
+  font-size: 60px
+
 `;
 export default Container
 export {}
