b88ea67629bdfa287079da762639988e5b98d40c
RakhmatilloRR <rrr.engineer.94@gmail.com>
2022-05-18 18:40:11 +0500

    first message

---
+++ src/pages/Simple/Simple.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Container } from './Style'
+
+export default function Simple() {
+  return (
+    <Container>Simple</Container>
+  )
+}
---
+++ src/root/Root.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Container from "./Style";
+
+export default function Root() {
+  return <Container>Root</Container>;
+}
