6e02a5f1dad87b91d96107b746337db61545bca6
RakhmatilloRR <rrr.engineer.94@gmail.com>
2022-05-19 13:27:54 +0500

    first message

--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Contacts from "../pages/Contacts/Contacts";
 import Main from "../pages/Main/Main";
@@ -10,10 +10,11 @@ export default function Root() {
   return (
     <Container>
       <Routes>
-        <Route path="/" element={<Navbar/>}>
-           <Route path="/home" element={<Main/>}/>
-           <Route path="/proporties" element={<Proporties/>}/>
-           <Route path="/contacts" element={<Contacts/>}/>
+        <Route path="/" element={<Navbar />}>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Main />} />
+          <Route path="/proporties" element={<Proporties />} />
+          <Route path="/contacts" element={<Contacts />} />
         </Route>
       </Routes>
     </Container>
